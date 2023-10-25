70c501d73e4842c9a5959bdfa24624b6feb52ad6
65x581 <148569623+65x581@users.noreply.github.com>
2023-10-25 16:29:00 -0400

    Create man.go

---
+++ frames/man.go
@@ -0,0 +1,23 @@
+package frames
+
+var Man = DefaultFrameType(manFrames)
+
+var manFrames = []string{
+	`                        #
+                      ###
+                       #
+                      # #
+                  `,
+
+	`                         #
+                       ###
+                        #
+                       # #
+                  `,
+
+	`                          #
+                        ###
+                         #
+                        # #
+                  `,
+}
