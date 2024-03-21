94e2294d86d37caacc0d83f664764791555b6749
DVSR1411 <d.v.sathwikreddy1411@gmail.com>
2024-03-21 23:58:18 +0530

    Create pacman.go

---
+++ frames/pacman.go
@@ -0,0 +1,57 @@
+package frames
+
+// Pacman frames stored in the FrameMap
+var Pacman = DefaultFrameType(pacmanFrames)
+var pacmanFrames = []string{
+	`  Oc;kkkk;c:o                                                      
+	O;x..........x'O                                                    
+   ;....xxx.xxx..x;o                                                    
+  k...xxxxxxx..xc                                                       
+ :.x..xxxxxx.xc      OcccO        :c;co       Occc:        :cccO        
+ ;....xxxxx.'o      olllllO      ;llll'       xllll:      :llll.       k
+ :..xxxxxxx...':      '...xo      :....;       k...xO      Ox...'       c
+  k..xxxxxxx...xc     ooo          ooo         ooo          ooo         
+   cx...xxxxxx...xc                                                     
+	oc'x.........x;o                                                    
+	   o:c;;;;c:O
+	`,
+
+	`  Oc;kkkk;cO                                                       
+	O;x..........xk:                                                    
+   ;x...xxxxxxxx....'O                                                  
+  k...xxxxxxxx....xx';                                                  
+ :.x..xxxxxx.xx'k;kk;co       oc;c:        :cccO        cccc        Occc
+ ;....xxxxx.x;O  :llllx       xllllc      Ollll.       kllllk       .lll
+ :..xxxxxxxxxxx';k....k       k....:      ox...'       c....c       '...
+  ;..xxxxxxxx.....x..;         ooo          ooo          oo          ooo
+   cx...xxxxxxxx....ko                                                  
+	oc'x.........x;O                                                    
+	   o:c;kk;c:O
+	`,
+
+	`  Oc;kkkk;c:o                                                      
+	O;x..........xk:                                                    
+   ;x...x.x..xxx....ko                                                  
+  k...xxxxxxxxxxxxx..xo                                                 
+ :.....xxxxxxxxxxxxxx.k    Oc;cO        :ccco       Occc:        :cccO  
+ ;.....xxxxxxxxxxxxxx.'   olllllo      ;llll'       xllll:      :llll.  
+ :..xxxxxxxxx.xxxxxx..;    x...xo      :....;       k...xO      Ox...'  
+  ;..xxxxxxxxxxxxx...'o     ooo          oo          ooo          ooo   
+   cx...xxxxxxx.....k                                                   
+	oc'x.........x;O                                                    
+	   o:c;;;;c:O
+	`,
+
+	`  Oc;kkkk;c:o                                                      
+	O;x..........xk:                                                    
+   ;....xx.xxxxx....'O                                                  
+  k...xxxxxxxxx...x'k:                                                  
+ :.xx.xxxxxxx..x;:o     :c;co       occc:        :cccO        cccc      
+ ;.xx.xxxxxx....o      cllllx       xllllc      Ollll.       kllllk     
+ :..xxxxxxxxx...k:o    :....k       k....O      ox...'       c....c     
+  ;..xxxxxxxxxxx..x'kO   ooo         ooo          ooo          oo       
+   cx...xxxxxxx.....ko                                                  
+	oc'x........xx;O                                                    
+	   o:c;;;;c:O 
+	`,
+}
