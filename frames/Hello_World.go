8253c3153bfad450b9684dca7dbb4b151742588d
vichub <95424057+vicgihub@users.noreply.github.com>
2024-04-30 19:25:46 +0200

    Add files via upload

---
+++ frames/Hello_World.go
@@ -0,0 +1,23 @@
+package frames
+
+var HelloWorld = DefaultFrameType(forrestFrames)
+
+var HelloWorldFrames = []string{
+	` H`,
+	` He`,
+	` Hel`,
+	` Hell`,
+	` Hello `,
+	` Hello W`,
+	` Hello Wo`,
+	` Hello Wor`,
+	` Hello Worl`,
+	` Hello World`,
+	` Hello Worl`,
+	` Hello Wor`,
+    ` Hello Wo`,
+    ` Hello W`,
+    ` Hello `,
+    ` Hell`,
+    ` Hel`,
+    ` He`}
