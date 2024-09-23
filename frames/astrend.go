67bbd33117522c6a5b2a03af1693a3fbfd833c49
Niccolas García Niño <niccogwngn@gmail.com>
2024-09-23 12:23:34 -0500

    Create astrend.go

---
+++ frames/astrend.go
@@ -0,0 +1,10 @@
+package frames
+
+// This is the value stored in the FrameMap
+var MyAnimation = DefaultFrameType(myAnimationFrames)
+
+var myAnimationFrames = []string{
+  `Frame1`,
+  `Frame2`,
+  `Frame3`,
+}
