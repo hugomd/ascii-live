ec5831678db1e20cae9116b601f0d6496f911d92
RohanPanchal-21 <panchalrohan64@gmail.com>
2025-01-25 19:35:53 +0530

    Added a new ascii animation teddy

---
+++ frames/teddy.go
@@ -0,0 +1,26 @@
+package frames
+
+var Teddy = DefaultFrameType(teddyFrames)
+
+var teddyFrames = []string{
+	`                        .cccc;;cc;';c.
+                      .,:dkdc:;;:c:,:d:.
+                     .loc'.,cc::::::,..,:.
+                   .cl;....;dkdccc::,...c;
+                  .c:,';:'..ckc',;::;....;c.
+                .c:'.,dkkoc:ok:;llllc,,c,';:.
+               .;c,';okkkkkkkk:,lllll,:kd;.;:,.
+               co..:kkkkkkkkkk:;llllc':kkc..oNc
+             .cl;.,okkkkkkkkkkc,:cll;,okkc'.cO;
+             ;k:..ckkkkkkkkkkkl..,;,.;xkko:',l'
+            .,...';dkkkkkkkkkkd;.....ckkkl'.cO;
+         .,,:,.;oo:ckkkkkkkkkkkdoc;;cdkkkc..cd,
+      .cclo;,ccdkkl;llccdkkkkkkkkkkkkkkkd,.c;
+     .lol:;;okkkkkxooc::loodkkkkkkkkkkkko'.oc
+   .c:'..lkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.oc
+  .lo;,ccdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkd,.c;
+,dx:..;lllllllllllllllllllllllllllllllloc'...
+cNO;........................................`,
+
+	
+}
