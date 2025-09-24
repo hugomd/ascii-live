c39696a467e49a9fed0d9c2f1dc3b95327305794
Hardik <picturesque.hardik@gmail.com>
2025-09-24 13:21:56 +0530

    Add beginner bouncing ball animation

---
+++ frames/bounce.go
@@ -0,0 +1,35 @@
+package frames
+
+var bounceFrames = []string{
+`       
+   OOO   
+   O O   
+   OOO   
+       
+       
+`,
+`       
+       
+   OOO   
+   O O   
+   OOO   
+       
+`,
+`       
+       
+       
+   OOO   
+   O O   
+   OOO   
+`,
+`       
+       
+       
+       
+   OOO   
+   O O   
+   OOO   
+`,
+}
+
+var Bounce = DefaultFrameType(bounceFrames)
