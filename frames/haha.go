7076b6848141147fa7c9f14300bdb9a96d476eca
maxdangerbg-oss <maxdanger.bg@gmail.com>
2026-03-21 14:54:06 -0400

    Create haha.go

---
+++ frames/haha.go
@@ -0,0 +1,28 @@
+package frames
+
+// This is the value stored in the FrameMap
+var MyAnimation = DefaultFrameType(myAnimationFrames)
+
+var myAnimationFrames = []string{
+  `I----------------    I            I    ___________     I    /`
+   I---------           I            I    I               I   /
+   I                    I            I    I               I  /
+   I                    I            I    I               I /
+   I                    I            I    I               I<
+   I                    I            I    I               I \
+   I                    I            I    I               I  \
+   I`                   I____________I    I___________    I   \,
+  `I----------------    I            I    ___________     I    /   \    /    I----------I    I            I
+   I---------           I            I    I               I   /     \  /     I          I    I            I
+   I                    I            I    I               I  /       \/      I          I    I            I
+   I                    I            I    I               I /         I      I          I    I            I
+   I                    I            I    I               I<          I      I          I    I            I
+   I                    I            I    I               I \         I      I          I    I            I
+   I                    I            I    I               I  \        I      I          I    I            I
+   I`                   I____________I    I___________    I   \       I      I----------I    I____________I
+  `__________________  ---------\   `
+                                 I 
+                                  >
+                                 I
+  ___________________  ---------/
+}
