babc74e179bba1b35c00e88af65fce0ea473ce1a
snas is sleepin <thuong0976@gmail.com>
2026-06-22 10:34:02 +0700

    Update frames.go

--- frames/frames.go
+++ frames/frames.go
@@ -63,4 +63,6 @@ var FrameMap = map[string]FrameType{
 	"kitty":           Kitty,
 	"india":           India,
 	"brittany":        Brittany,
+	"frisk":           FriskDance,
+	"sans":            Sans10thanniversary,
 }
