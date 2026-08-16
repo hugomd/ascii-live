8c5fbdede72764c69fe7a856e8596348624d5bdc
chowddogramareadepot-boop <chowddogramareadepot@gmail.com>
2026-08-16 13:14:45 +0600

    Create bangladesh.go
    bangladesh.go

---
+++ frames/bangladesh.go
@@ -0,0 +1,30 @@
+package frames
+
+var Bangladesh = DefaultFrameType(bangladeshFrames)
+
+var bangladeshFrames = []string{
+    `
+     ========================================
+             BANGLADESH - ASCII MAP
+     ========================================
+                  _.-^^---....,,--       
+              _--Î              --_  
+             <        BANGLADESH    >)
+             |                         | 
+              \_                       _/
+                -._                 _.-  
+                   ---....,,--....-    
+    `,
+    `
+     ========================================
+             BANGLADESH - LIVE STREAM
+     ========================================
+                  _.-^^---....,,--       
+              _--Î   * Dhaka    --_  
+             <       * Sylhet      >)
+             |       * Chittagong     | 
+              \_     * Khulna          _/
+                -._                 _.-  
+                   ---....,,--....-    
+    `,
+}FrameMap["bangladesh"] = Bangladesh
