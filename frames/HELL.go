0b85bdfe397a8c6c67197e46900d329de070ce1d
rewwww8 <zhougjordan@gmail.com>
2026-08-18 21:54:05 +0200

    Create HELL.go

---
+++ frames/HELL.go
@@ -0,0 +1,27 @@
+package frames
+
+// This is the value stored in the FrameMap
+var MyAnimation = DefaultFrameType(myAnimationFrames)
+
+var myAnimationFrames = []string{ 
+`__  __    
+/\ \_\ \   
+\ \  __ \  
+ \ \_\ \_\ 
+  \/_/\/_/`,
+  ` ______    
+   /\  ___\   
+   \ \  __\   
+    \ \_____\ 
+     \/_____/`,
+  ` __        
+   /\ \       
+   \ \ \____  
+    \ \_____\ 
+     \/_____/`,
+  ` ______  
+   /\  == \ 
+   \ \  _-/ 
+    \ \_\   
+     \/_/ `,
+}
